80d03dd3c76d6a11f457a497e0c8e9b26d835db1
Valentine Rybin <valentine.rybin@gmail.com>
2016-12-09 01:26:07 +0300

    First working version
    Now click on button opens new tab
    with list of urls of all tabs of current window.

--- buildpage.js
+++ buildpage.js
@@ -1,6 +1,8 @@
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+var myPort = browser.runtime.connect();
+
+myPort.onMessage.addListener(function(m) {
     document.body.textContent = "";
-    for (let url of request.urls) {
+    for (let url of m.urls) {
         let p = document.createElement("p");
         p.textContent = url;
         document.body.appendChild(p);
--- list_tabs_firefox.js
+++ list_tabs_firefox.js
@@ -1,20 +1,28 @@
+var portFromCS;
+
 browser.browserAction.onClicked.addListener(function() {
-    browser.tabs.create({active: true, url: "about:blank"}).then(function(current_tab) {
-        var tabs_list = [];
-        browser.tabs.query({currentWindow: true}).then(function(response_tabs) {
-            tabs_list = response_tabs;
-        });
-
-        browser.tabs.query({currentWindow: true, active: true}).then(function(response_tabs) {
-            let currenttab = response_tabs[0];
-            browser.tabs.executeScript(currenttab.id, {file: "buildpage.js"});
-
-            var url_list = [];
-            for (let tab of tabs_list) {
-                url_list.push(tab.url);
-            }
-
-            browser.tabs.sendMessage(currenttab.id, {urls: url_list});
-        });
-    });
+    browser.tabs.create({active: true, url: "/blank.html"}).then(onCreated);
 });
+
+function onCreated(tab) {
+  browser.runtime.onConnect.addListener(connected);
+}
+
+function onReceivedTabs(tabs) {
+    var url_list = [];
+    var activeTabId;
+    for (let tab of tabs) {
+        url_list.push(tab.url);
+        if (tab.active == true) {
+            activeTabId = tab.id;
+        }
+    }
+   portFromCS.postMessage({urls: url_list});
+}
+
+function connected(p) {
+  portFromCS = p;
+  browser.tabs.query({currentWindow: true}).then(onReceivedTabs);
+}
+
+browser.runtime.onConnect.addListener(connected);
